113b1a0a5809636beb1496ecf14b8e25a390bc40
Timofey Goryaynov <119266784+CPUtester5465@users.noreply.github.com>
2023-08-27 14:13:38 +0300

    Update master.sh

--- sh-scripts/master.sh
+++ sh-scripts/master.sh
@@ -1,9 +1,9 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt update
+sudo apt update 
 sudo apt install python ansible -y
-sudo apt-get install acl
+sudo apt-get install acl -y
 
 sudo useradd -m -s /bin/bash kube-cluster
 sudo passwd kube-cluster -d
