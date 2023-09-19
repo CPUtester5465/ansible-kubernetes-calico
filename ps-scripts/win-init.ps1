33a9e6f64d54a4c4fe81fd8895769b49d66a0601
Timofey Goryaynov <119266784+CPUtester5465@users.noreply.github.com>
2023-09-19 14:27:11 +0300

    Update win-init.ps1

--- ps-scripts/win-init.ps1
+++ ps-scripts/win-init.ps1
@@ -1,22 +1,24 @@
 $Username = "ansible"
+$Password = ""
+
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
 
 Start-Transcript -Path "C:\Users\$Username\Desktop\init_ps.log"
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ansible/ansible/stable-2.10/examples/scripts/ConfigureRemotingForAnsible.ps1' -OutFile "C:\Users\$Username\Desktop\ConfigureRemotingForAnsible.ps1"
-C:\Users\$Username\Desktop\ConfigureRemotingForAnsible.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
-choco install git -yes
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-RefreshEnv
-git config --global user.name "test"
-git config --global user.email "test@example.com"
+Start-Process PowerShell -Credential $Credential -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File C:\Users\$Username\Desktop\ConfigureRemotingForAnsible.ps1"
+$scriptBlock = {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+    choco install git -yes
+    Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+    RefreshEnv
+}
+
+Start-Process PowerShell -Credential $Credential -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $scriptBlock"
 mkdir C:\Users\$Username\.ssh
 
-$Password = ConvertTo-SecureString $Password -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential ($Username, $Password)
-
-Start-Process -FilePath "ssh-keygen" -ArgumentList "-f C:\Users\$Username\.ssh\id_rsa -t rsa -N '""'" -Credential $Credential
+ssh-keygen -f C:\Users\ansible\.ssh\id_rsa -t rsa -N '""'
 
 mkdir C:\Users\$Username\repo
-type C:\Users\$Username\.ssh\id_rsa.pub
-
-# send id_rsa.pub or C:\Users\$Username\Desktop\init_ps.log
